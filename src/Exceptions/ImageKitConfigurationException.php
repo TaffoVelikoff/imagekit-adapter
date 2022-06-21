0c62a86e85b7e40e5bd11a421b7a9909bd325a9e
Taffo Velikoff <contact@taffovelikoff.com>
2022-06-21 14:46:09 +0300

    The whole adapter has been rewritten to be compatible with the new Flysystem v3.

---
+++ src/Exceptions/ImageKitConfigurationException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TaffoVelikoff\ImageKitAdapter\Exceptions;
+
+use Exception;
+
+/**
+ * 
+ */
+class ImageKitConfigurationException extends Exception
+{
+    //
+}
--- src/ImagekitAdapter.php
+++ src/ImagekitAdapter.php
@@ -3,204 +3,91 @@
 namespace TaffoVelikoff\ImageKitAdapter;
 
 use DateTime;
-use League\Flysystem\Adapter\AbstractAdapter;
-use League\Flysystem\Adapter\Polyfill\NotSupportingVisibilityTrait;
+use League\Flysystem;
 use League\Flysystem\Config;
-use League\Flysystem\Util\MimeType;
+use League\Flysystem\DirectoryAttributes;
+use League\Flysystem\FileAttributes;
+use League\Flysystem\UnableToCheckDirectoryExistence;
+use League\Flysystem\UnableToCreateDirectory;
+use League\Flysystem\UnableToReadFile;
+use League\Flysystem\UnableToWriteFile;
+use League\MimeTypeDetection\FinfoMimeTypeDetector;
+use TaffoVelikoff\ImageKitAdapter\Exceptions\ImageKitConfigurationException;
 
-class ImagekitAdapter extends AbstractAdapter {
-
-	use NotSupportingVisibilityTrait;
+class ImagekitAdapter implements Flysystem\FilesystemAdapter {
 
     protected $client;
+    protected $options;
 
-    public function __construct(\ImageKit\ImageKit $client) {
+    public function __construct(\ImageKit\ImageKit $client, $options = []) {
         $this->client = $client;
-    }
-	
-    public function getUrl($path)
-    {
-        return $this->client->url([
-                "path" => $path,
-            	]);
+        $this->options = $options;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function write($path, $contents, Config $config)
+    public function fileExists(string $path): bool
     {
-        return $this->upload($path, $contents);
-    }
+        $location = $this->getFileFolderNames($path);
 
-    /**
-     * {@inheritdoc}
-     */
-    public function writeStream($path, $resource, Config $config)
-    {
-        return $this->upload($path, $resource);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function update($path, $contents, Config $config)
-    {
-        // Make a purge cache request
-        if(config('imagekit.purge_cache_update') === true)
-            $this->client->purgeCacheApi(config('imagekit.endpoint').'/'.$path);
-
-        return $this->upload($path, $contents);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function updateStream($path, $resource, Config $config)
-    {
-        return parent::updateStream($path, $resource, $config);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function rename($path, $newPath): bool
-    {   
-        // Get old file
-        $oldFile = $this->searchFile($path);
-        $oldFileUrl = $oldFile->url;
-
-        // Upload new file
-        $new = $this->upload($newPath, $oldFileUrl);
-
-        // Delete old file
-        $this->client->deleteFile($oldFile->fileId);
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function copy($path, $newPath): bool
-    {
-        $oldFile = $this->searchFile($path);
-        $oldFileUrl = $oldFile->url;
-
-        // Upload new file
-        $new = $this->upload($newPath, $oldFileUrl);
-
-        if($new->err != null)
-            return false;
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function delete($path): bool
-    {
-        // Find file
-        $file = $this->searchFile($path);
-
-        // Make a purge cache request
-        if(config('imagekit.purge_cache_update') === true)
-            $this->client->purgeCacheApi(config('imagekit.endpoint').'/'.$path);
-
-        // Delete file
-        $delete = $this->client->deleteFile($file->fileId);
-
-        if($delete->err == null)
-            return true;
-
-        return false;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function deleteDir($dirname): bool
-    {
-        // Get the folder
-        $folder = $this->client->listFiles([
-            'name'          => $dirname,
-            'includeFolder' => true
-        ]);
-
-        if(count($folder->success) == 0)
-            return false;
-
-        // Delete folder
-        $this->client->deleteFile($folder->success[0]->folderId);
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function createDir($dirname, Config $config)
-    {
-        // The ImageKit API does not offer an endpoint for this acion currently.
-        // A workaround is to upload an "empty" file, the upload endpoint can create
-        // folders when the "folder" parameter is set.
-
-        // Upload file
-        $upload = $this->client->upload([
-            'file'              => 'xxx',
-            'fileName'          => 'empty',
-            'useUniqueFileName' => false,
-            'folder'            => $dirname
-        ]);
-
-        return $upload;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function has($path)
-    {
-        // File name and directory
-        $filePath = $this->getFileFolderName($path);
-
-        // Search for file
         $file = $this->client->listFiles([
-            'name'          => $filePath['fileName'],
-            'path'          => $filePath['directory'],
+            'name'          => $location['file'],
             'includeFolder' => true
         ]);
 
-        // Does NOT exist
-        if(count($file->success) == 0)
+        if(empty($file->success))
             return false;
 
-        // Exists
         return true;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function read($path)
+    public function directoryExists(string $path): bool
     {
-        $filePath = $this->getFileFolderName($path);
+        $location = $this->getFileFolderNames($path);
 
-        // Get file
-        $file = $this->client->listFiles([
-            'name'          => $filePath['fileName'],
+        $directory = $this->client->listFiles([
+            'name'          => $location['file'],
             'includeFolder' => true
         ]);
 
-        // Convert result to array
-        $data = (array)$file;
+        if(empty($directory->success))
+            return false;
 
-        // Return file url
-        return [
-            'contents'  => $data['success'][0],
-            'stream'    => null,
-        ];
+        if($directory->success[0]->type != 'folder')
+            throw new UnableToCheckDirectoryExistence;
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write(string $path, $contents, Config $config): void
+    {
+        if($this->upload($path, $contents, $config) == false)
+            throw new UnableToWriteFile('Unable to write file to location '.$path);
+            
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function writeStream(string $path, $contents, Config $config): void
+    {
+        if($this->upload($path, $contents, $config) == false)
+            throw new UnableToWriteFile('Unable to write file to location '.$path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read(string $path): string
+    {
+        return stream_get_contents($this->readStream($path));
     }
 
     /**
@@ -208,165 +95,173 @@ class ImagekitAdapter extends AbstractAdapter {
      */
     public function readStream($path)
     {
-        return $this->read($path);
+        $location = $this->getFileFolderNames($path);
+
+        $file = $this->client->listFiles([
+            'name'          => $location['file'],
+            'includeFolder' => true
+        ]);
+        
+        return @fopen($file->success[0]->url, 'rb');
     }
 
     /**
      * {@inheritdoc}
      */
-    public function listContents($directory = '', $recursive = false): array
+    public function delete(string $path): void
+    {
+
+        if(!strlen($path))
+            throw new UnableToReadFile;
+
+        $file = $this->searchFile($path);
+
+        // Make a purge cache request
+        if(isset($this->options['purge_cache_update']) && $this->options['purge_cache_update']['enabled'] === true) {
+            
+            if(!isset($this->options['purge_cache_update']['endpoint_url']))
+                throw new ImageKitConfigurationException('Purge cache option is enabled, but endpoint url is not set.');
+
+            $this->client->purgeCacheApi($this->options['purge_cache_update']['endpoint_url'].'/'.$path);
+        
+        }
+
+        $this->client->deleteFile($file->fileId);
+        
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function deleteDirectory(string $path): void
+    {
+        $delete = $this->client->deleteFolder($path);
+
+        if($delete->err != null)
+            throw new UnableToReadFile('Directory not found.');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createDirectory(string $path, Config $config): void
+    {
+        $create = $this->client->createFolder($path);
+
+        if(empty($create->success))
+            throw new UnableToCreateDirectory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setVisibility(string $path, string $visibility): void
+    {
+        throw UnableToSetVisibility::atLocation($path, 'Adapter does not support visibility controls.');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function visibility(string $path): FileAttributes
+    {
+        throw UnableToSetVisibility::atLocation($path, 'Adapter does not support visibility controls.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function mimeType(string $path): FileAttributes
+    {
+        return new FileAttributes($path, null, null, null, (new FinfoMimeTypeDetector())->detectMimeTypeFromPath($path));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function lastModified(string $path): FileAttributes
+    {
+        $file = $this->searchFile($path);
+
+        $meta = $this->client->getFileDetails($file->fileId);
+
+        return new FileAttributes($path, null, null, strtotime($meta->success->updatedAt));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function fileSize(string $path): FileAttributes
+    {
+
+        $file = $this->searchFile($path);
+
+        $meta = $this->client->getMetaData($file->fileId);
+
+        return new FileAttributes($path, $meta->success->size ?? null );
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function listContents(string $path, bool $deep = false): iterable
     {
 
         $list = $this->client->listFiles([
-            'name'          => $directory,
-            'includeFolder' => config('imagekit.include_folders')
+            'path'          => $path,
+            'includeFolder' => $deep
         ]);
 
-        // If not recursive remove files
-        if($recursive === false) {
-            foreach ($list->success as $key => $e) {
-
-                // Get path parts
-                if(isset($e->filePath)) {
-                    $pathParts = explode('/', $e->filePath);
-                } else {
-                    $pathParts = explode('/', $e->folderPath);
-                }
-
-                // Get directory name
-                end($pathParts);
-                $dirName = prev($pathParts);
-
-                if($dirName != $directory) {
-                    unset($list->success[$key]);
-                }
-            }
+        foreach ($list->success as $item) {
+            yield $this->normalizeObject($item);
         }
-
-        $files = array_map(function($e) use($recursive, $directory) {
-
-            $dirName = '';
-
-            // Get path parts
-            if(isset($e->filePath)) {
-                $pathParts = explode('/', $e->filePath);
-                $filePath = $e->filePath; 
-            } else {
-                $pathParts = explode('/', $e->folderPath);
-                $filePath = $e->folderPath;
-            }
-
-            // Get directory name
-            end($pathParts);
-            $dirName = prev($pathParts);
-
-            return [
-                'path'      => $filePath,
-                'dirname'   => $dirName,
-            ];
-            
-        }, $list->success);
-
-        return $files;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getMetadata($path)
+    public function copy(string $source, string $destination, Config $config): void
     {
-        // Search for file
-        $file = $this->searchFile($path);
 
-        // Get data from file search
-        $return = (array)$file;
+        $sourceFile = $this->searchFile($source);
+
+        $this->upload($destination, $sourceFile->url);
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function move(string $source, string $destination, Config $config): void
+    {
+
+        $path = $this->getFileFolderNames($source);
         
-        $fileId = $file->fileId;
-
-        // Get timestamp
-        $createdAt = $file->createdAt;
-        $date = new DateTime($createdAt);
-        $timestamp = $date->getTimestamp();
-        $return['timestamp'] = $timestamp;
-
-        // Get mimetype
-        $return['mimetype'] = MimeType::detectByFilename($path);
-
-        // Get more meta data
-        $moreData = $this->client->getMetaData($fileId);
-        $return = array_merge((array)$moreData->success, $return);
-
-        // Return all
-        return $return;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getSize($path)
-    {
-        return $this->getMetadata($path);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getMimetype($path)
-    {
-        return $this->getMetadata($path);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getTimestamp($path)
-    {
-        return $this->getMetadata($path);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function applyPathPrefix($path): string
-    {
-        $path = parent::applyPathPrefix($path);
-
-        return '/'.trim($path, '/');
-    }
-
-    /**
-     * @param string $path
-     * @param resource|string $contents
-     *
-     * @return array|false file metadata
-     */
-    protected function upload(string $path, $contents)
-    {
-        $file = $this->getFileFolderName($path);
-        
-        if($file === false)
-            return false;
-
-        // Upload file
-        $upload = $this->client->upload([
-            'file'              => $contents,
-            'fileName'          => $file['fileName'],
-            'useUniqueFileName' => false,
-            'folder'            => $file['directory']
+        $list = $this->client->listFiles([
+            'name'  => $path['file'],
+            'path'  => $path['directory'],
+            'includeFolder' => true
         ]);
 
+        if(empty($list->success))
+            throw new UnableToReadFile('File or directory not found.');
 
-        return $upload;
+        if($list->success[0]->type == 'folder') {
+            $move = $this->client->moveFolder($source, $destination);
+        } else {
+            $move = $this->client->moveFile($source, $destination);
+        }
     }
 
     /**
-     * @param string $path
+     * Transform $path to file name and folder name.
+     * @param string $path Path to file
      *
      * @return array|false
-     *
-     * Transform $path to file name and folder name
      */
-    public function getFileFolderName(string $path) {
+    public function getFileFolderNames(string $path) 
+    {
 
         if(!$path)
             return false;
@@ -382,26 +277,107 @@ class ImagekitAdapter extends AbstractAdapter {
         }
 
         return [
-            'fileName'  => $fileName,
+            'file'  => $fileName,
             'directory' => $folder
         ];
     }
 
-     /**
+
+    /**
+     * @param array $item Files and folders from listContents.
+     *
+     * @return StorageAttributes
+     */
+    protected function normalizeObject(object $item): StorageAttributes
+    {
+        return match ($item->type) {
+
+            'folder' => new DirectoryAttributes(
+                $item->folderPath,
+                null,
+                strtotime($item->updatedAt),
+                ['id'   => $item->folderId]
+            ),
+
+            'file' => new FileAttributes(
+                $item->filePath,
+                $item->size,
+                null,
+                strtotime($item->updatedAt),
+                $item->mime ?? null,
+                ['id'   => $item->fileId]
+            )
+
+        };
+    }
+
+    /**
+     * Upload a file to Imagekit.io
+     * @param string $path
+     * @param resource|string $contents
+     *
+     * @return mixed file metadata
+     */
+    protected function upload(string $path, $contents) 
+    {
+
+        $location = $this->getFileFolderNames($path);
+
+        if($location === false)
+            return false;
+
+        // If not resource or URL - base64 encode
+        if(!is_resource($contents) && !filter_var($contents, FILTER_VALIDATE_URL))
+            $contents = base64_encode($contents);
+
+        $upload = $this->client->upload([
+            'file'              => $contents,
+            'fileName'          => $location['file'],
+            'useUniqueFileName' => false,
+            'folder'            => $location['directory']
+        ]);
+
+        return $upload;
+
+    }
+
+    /**
+     * Search for a file or directory by name/path.
      * @param string $path
      *
-     * Search for a file or directory by name/path
+     * @return array
      */
-    public function searchFile($path) {
-        $filePath = $this->getFileFolderName($path);
+    public function searchFile($path) 
+    {
+
+        $location = $this->getFileFolderNames($path);
 
         // Get file from old path
         $file = $this->client->listFiles([
-            'name'    => $filePath['fileName'],
-            'path'    => $filePath['directory']
+            'name'    => $location['file'] ?? '',
+            'path'    => $location['directory'] ?? ''
         ]);
 
+        if(empty($file->success))
+            throw new UnableToReadFile('File or directory not found.');
+
         return $file->success[0];
+
+    }
+
+    /**
+     * Get the full url.
+     * @param string $path
+     *
+     * @return array
+     */
+    public function getUrl(string $path): string 
+    {
+
+        return $this->client->url([
+            "path" => $path,
+        ]);
+
     }
 
 }
